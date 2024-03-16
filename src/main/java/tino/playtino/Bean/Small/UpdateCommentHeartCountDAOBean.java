2af4252955a34971727f95019ddbc3446872b083
niireymik <yeah_0911@tukorea.ac.kr>
2024-03-17 03:27:10 +0900

    [ 댓글 좋아요 추가/삭제 ] : UpdateCommentHeartCountDAOBean 에서 JpaCommentRepository 의존성 제거

--- src/main/java/tino/playtino/Bean/Small/UpdateCommentHeartCountDAOBean.java
+++ src/main/java/tino/playtino/Bean/Small/UpdateCommentHeartCountDAOBean.java
@@ -3,25 +3,24 @@ package tino.playtino.Bean.Small;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import tino.playtino.domain.Comment;
-import tino.playtino.repository.JpaCommentRepository;
 
 import java.util.UUID;
 
 @Component
 public class UpdateCommentHeartCountDAOBean {
 
-    JpaCommentRepository jpaCommentRepository;
+    GetCommentDAOBean getCommentDAOBean;
 
     @Autowired
-    public UpdateCommentHeartCountDAOBean(JpaCommentRepository jpaCommentRepository){
-        this.jpaCommentRepository = jpaCommentRepository;
+    public UpdateCommentHeartCountDAOBean(GetCommentDAOBean getCommentDAOBean){
+        this.getCommentDAOBean = getCommentDAOBean;
     }
 
     //댓글좋아요 추가(+1)
     public Comment heartCountUp(UUID commentId){
 
         //commentId로 해당 Comment(DAO) 찾기
-        Comment comment = jpaCommentRepository.findById(commentId).get();
+        Comment comment = getCommentDAOBean.exec(commentId);
 
         //DAO의 댓글좋아요개수(HeartCount) +1
         comment.setHeartCount(comment.getHeartCount()+1);
@@ -34,7 +33,7 @@ public class UpdateCommentHeartCountDAOBean {
     public Comment heartCountDown(UUID commentId){
 
         //commentId로 해당 Comment(DAO) 찾기
-        Comment comment = jpaCommentRepository.findById(commentId).get();
+        Comment comment = getCommentDAOBean.exec(commentId);
 
         //댓글좋아요개수(HeartCount) -1
         comment.setHeartCount(comment.getHeartCount()-1);
